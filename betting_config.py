7f342c7189837610442345806e00a5084893d7cb
GmaninSA <Gboydjr0694@gmail.com>
2025-07-08 17:03:44 -0500

    Update betting_config.py

--- betting_config.py
+++ betting_config.py
@@ -1,11 +1,8 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
-
-ODDS_API_KEY = os.getenv("ODDS_API_KEY")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+ODDS_API_KEY = st.secrets["ODDS_API_KEY"]
+TELEGRAM_BOT_TOKEN = st.secrets["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_CHAT_ID = st.secrets["TELEGRAM_CHAT_ID"]
 
 BANKROLL_START = 1000
 BET_SIZE = 50
